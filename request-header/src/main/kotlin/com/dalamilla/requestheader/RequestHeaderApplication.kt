8a3f280cdabf8390104d8b75cb1bc7e1fb95d658
Daniel Alamilla <daniel.alamilla.co@gmail.com>
2022-04-11 20:10:56 -0500

    add request header project

--- request-header/src/main/kotlin/com/dalamilla/requestheader/RequestHeaderApplication.kt
+++ request-header/src/main/kotlin/com/dalamilla/requestheader/RequestHeaderApplication.kt
@@ -2,10 +2,28 @@ package com.dalamilla.requestheader
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.*
+import javax.servlet.http.HttpServletRequest
 
 @SpringBootApplication
 class RequestHeaderApplication
 
 fun main(args: Array<String>) {
-	runApplication<RequestHeaderApplication>(*args)
+    runApplication<RequestHeaderApplication>(*args)
 }
+
+@RestController
+class MessageResource {
+
+    @CrossOrigin(origins = ["*"])
+    @GetMapping("/api/whoami")
+    fun whoami(
+        request: HttpServletRequest,
+        @RequestHeader("Accept-Language") language: String,
+        @RequestHeader("User-Agent") software: String,
+
+
+        ): Message = Message(request.remoteAddr, language, software)
+}
+
+data class Message(val ipaddress: String, val language: String, val software: String)
--- request-header/src/test/kotlin/com/dalamilla/requestheader/RequestHeaderApplicationTests.kt
+++ request-header/src/test/kotlin/com/dalamilla/requestheader/RequestHeaderApplicationTests.kt
@@ -1,13 +1,35 @@
 package com.dalamilla.requestheader
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
-@SpringBootTest
-class RequestHeaderApplicationTests {
+@WebMvcTest
+class RequestHeaderApplicationTests(@Autowired val mockMvc: MockMvc) {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun checkPath() {
+        mockMvc.perform(
+            get("/api/whoami").header("Accept-Language", "en-US,en;q=0.9,es;q=0.8")
+                .header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64")
+        )
+            .andExpect(status().isOk)
+    }
 
+    @Test
+    fun checkValues() {
+        mockMvc.perform(
+            get("/api/whoami").header("Accept-Language", "en-US,en;q=0.9,es;q=0.8")
+                .header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64")
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.software").value("Mozilla/5.0 (X11; Linux x86_64"))
+            .andExpect(jsonPath("$.language").value("en-US,en;q=0.9,es;q=0.8"))
+            .andExpect(jsonPath("$.ipaddress").value("127.0.0.1"))
+    }
 }
